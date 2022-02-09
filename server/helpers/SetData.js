b48b972294cc7315b3d9e1e49e843b9250a9a21f
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-02-09 21:16:03 +0100

    FIX - fix filter in `setData`

--- server/helpers/SetData.js
+++ server/helpers/SetData.js
@@ -1,7 +1,20 @@
+const { filter, isEmpty } = require("lodash");
 const data = require("../data/games.json");
 
-function dataSet() {
-  return data;
+function dataSet(genre, platform) {
+  return filter(data, (x) => {
+    let condition1 = true;
+    let condition2 = true;
+    if (!isEmpty(genre)) {
+      condition1 = x.genre.toLowerCase().match(genre.toLocaleLowerCase());
+    }
+    if (!isEmpty(platform)) {
+      condition2 = x.platforms.some((y) =>
+        y.toLowerCase().match(platform.toLocaleLowerCase())
+      );
+    }
+    return condition1 && condition2;
+  });
 }
 
 module.exports = dataSet;
