5cf89ca1328928f3f84a4e144a1f3f9140460e59
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-02-09 22:20:47 +0100

    FIX - `SelectTopByPlayers` component

--- client/src/components/SelectTopByPlayers.js
+++ client/src/components/SelectTopByPlayers.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import List from "../utils/List";
-import Search from "../utils/Search";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, TextField } from "@material-ui/core";
+import axios from "axios";
+
+const useStyles = makeStyles(() => ({
+  marginTop: {
+    marginTop: 20,
+  },
+}));
 
 function SelectTopByPlayers() {
+  const classes = useStyles();
+  const [genre, setGenre] = useState("");
+  const [platform, setPlatform] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    return axios
+      .get(`${process.env.REACT_APP_BASE_URL}/select_top_by_players`, {
+        params: { genre, platform },
+      })
+      .then(({ data }) => setData(data))
+      .catch((err) => console.error(err));
+  }, [genre, platform]);
+
   return (
     <>
-      <Search />
-      <List />
+      <Grid
+        container
+        spacing={3}
+        justifyContent="flex-end"
+        alignItems="center"
+        className={classes.marginTop}
+      >
+        <Grid item>
+          <TextField
+            label="Genre"
+            type="search"
+            variant="outlined"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            label="Platform"
+            type="search"
+            variant="outlined"
+            value={platform}
+            onChange={(e) => setPlatform(e.target.value)}
+          />
+        </Grid>
+      </Grid>
+      <List data={data} />
     </>
   );
 }
