2e66764de3b5f2843ddb23705b9fe7b532660d00
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-02-09 21:03:47 +0100

    feat - add `.env`

--- server/index.js
+++ server/index.js
@@ -6,15 +6,14 @@ require("dotenv").config();
 
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('hello world !');
-})
+app.get("/", (req, res) => {
+  res.send("hello world !");
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port: http://localhost:${process.env.PORT}`);
-  });
+  console.log(`listening on port: http://localhost:${process.env.PORT}`);
+});
